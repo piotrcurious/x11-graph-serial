82e8f304f3c6ff56cacc7a598cd1022b4c39904a
piotrcurious <78758456+piotrcurious@users.noreply.github.com>
2023-05-12 16:14:41 -0400

    Add files via upload
    added color theme and some fixes and improvements

--- serial_plotter_resize_event.c
+++ serial_plotter_resize_event.c
@@ -16,12 +16,15 @@
 #include <ev.h>
 //#include <readline.h>
 
+#define BAUD_RATE B115200
+
 #define MAX_DATA_FIELDS 8 // Maximum number of data fields to plot
 #define WINDOW_WIDTH 800 // Initial window width
 #define WINDOW_HEIGHT 600 // Initial window height
 //#define DATA_POINT_CIRCLE // wheter to draw a circle on each data point position (slow)
 #define MARGIN 20 // Margin around the graph
 #define INTERNAL_GRAPH_MARGIN 0.001 // Margin for min/max values
+
 #define COLOR_BLACK 0 // Color index for black
 #define COLOR_RED 1 // Color index for red
 #define COLOR_GREEN 2 // Color index for green
@@ -29,16 +32,17 @@
 #define COLOR_YELLOW 4 // Color index for yellow
 #define COLOR_MAGENTA 5 // Color index for magenta
 #define COLOR_CYAN 6 // Color index for cyan
-#define COLOR_WHITE 7 // Color index for white
+#define COLOR_GRAY 7 // Color index for white
+#define COLOR_WHITE 8 // Color index for white
 
 // A structure to store a data point
 typedef struct {
-    unsigned long timestamp; // Timestamp in milliseconds
+    uint32_t timestamp; // Timestamp in milliseconds
     float values[MAX_DATA_FIELDS]; // Data values
 } DataPoint;
 
-#define MAX_DATA_POINTS 1024 // Maximum number of data points to store 
-#define DISCARD_DATA_POINTS 2 // amount of data points to discard to synchronize with source
+#define MAX_DATA_POINTS 2048 // Maximum number of data points to store 
+#define DISCARD_DATA_POINTS 3 // amount of data points to discard to synchronize with source
 #define LINE_SIZE 256 // max line size (line buffer)
 
 // A structure to store the graph parameters
@@ -46,8 +50,8 @@ typedef struct {
     uint16_t width; // Window width
     uint16_t height; // Window height
     uint8_t num_fields; // Number of data fields to plot
-    uint64_t min_timestamp; // Minimum timestamp in the data
-    uint64_t max_timestamp; // Maximum timestamp in the data
+    uint32_t min_timestamp; // Minimum timestamp in the data
+    uint32_t max_timestamp; // Maximum timestamp in the data
     float min_value; // Minimum value in the data
     float max_value; // Maximum value in the data
     int colors[MAX_DATA_FIELDS]; // Colors for each data field
@@ -62,7 +66,7 @@ GC gc;
 // A global variable to store the color map
 Colormap colormap;
 // A global variable to store the color pixels
-unsigned long pixels[8];
+unsigned long pixels[9]; // 9 because 9 colors in the palette. 
 // A global variable to store the serial port file descriptor
 int serial_fd;
 // A global variable to store the data buffer
@@ -76,6 +80,8 @@ Bool keypress = False;
 // A function to initialize the X11 display and window
 Bool new_serial_data = False; 
 // a global variable to indicate new serial data arrived
+uint8_t color_theme = 0; 
+// a global variable to store color theme
 
 DataPoint data_point;
 // a global structure for data point
@@ -107,6 +113,8 @@ void init_x11(char *title) {
     pixels[COLOR_MAGENTA] = color.pixel;
     XAllocNamedColor(display, colormap, "cyan", &color, &color);
     pixels[COLOR_CYAN] = color.pixel;
+    XAllocNamedColor(display, colormap, "Gray41", &color, &color);
+    pixels[COLOR_GRAY] = color.pixel;
     XAllocNamedColor(display, colormap, "white", &color, &color);
     pixels[COLOR_WHITE] = color.pixel;
 
@@ -140,6 +148,17 @@ void init_x11(char *title) {
 
     XSetBackground(display, gc,
                    pixels[COLOR_WHITE]);
+
+    // Assign different colors to each data field
+    graph.colors[0] = COLOR_RED;
+    graph.colors[1] = COLOR_GREEN;
+    graph.colors[2] = COLOR_BLUE;
+    graph.colors[3] = COLOR_YELLOW;
+    graph.colors[4] = COLOR_MAGENTA;
+    graph.colors[5] = COLOR_CYAN;
+    graph.colors[6] = COLOR_BLACK;
+    graph.colors[7] = COLOR_GRAY;
+
     // Map the window on the screen and flush the output buffer
     XMapWindow(display, window);
     XFlush(display);
@@ -187,7 +206,6 @@ void init_serial(char *device, int baud) {
 
     // Set the terminal attributes of the serial port
     tcsetattr(serial_fd, TCSANOW, &options);
-
 }
 
 // A function to close the serial port
@@ -208,42 +226,39 @@ int read_data_point(DataPoint *data_point) {
         // Read one character from the serial port
         char c;
         int n = read(serial_fd, &c, 1);
-        // Check for end of file or error
-        if (n == 0) {
-            return 0;
+
+        if (n == 0) { //if no new data
+//            return 0;
+	usleep (50); // wait a little for the buffer to fill up. 
+	// TODO : implement timeout
         }
 
         if (n == -1) {
             fprintf(stderr, "Error: Cannot read from serial port\n");
-            return -1;
+//            return -1;
         }
-
         // Check for newline or buffer overflow
         if (c == '\n' || index == 255) {
             break;
         }
-
-//        // Append the character to the buffer
+        // Append the character to the buffer
+	if (n == 1 ) { // if there is data
          line[index++] = c;
+	}
     }
-
     // Check if the line buffer is empty
      if (index == 0 ) {
          return 0 ;
      }
-
     // Check if the line buffer has a trailing CR character and remove it
      if (line [index - 1] == '\r') {
          index--;
      }
-
     // Terminate the buffer with a null character
     line[index] = '\0';
-
     // Parse the buffer as a comma-separated list of values
     char *token ;
     token = strtok(line, ",");
-
     // The first token should be the timestamp in milliseconds
     if (token == NULL) {
         fprintf(stderr, "Error: Invalid data format\n");
@@ -284,15 +299,16 @@ void update_graph() {
     graph.min_value = 0;
     graph.max_value = 1;
 
-    // Assign different colors to each data field
-    graph.colors[0] = COLOR_RED;
-    graph.colors[1] = COLOR_GREEN;
-    graph.colors[2] = COLOR_BLUE;
-    graph.colors[3] = COLOR_YELLOW;
-    graph.colors[4] = COLOR_MAGENTA;
-    graph.colors[5] = COLOR_CYAN;
-    graph.colors[6] = COLOR_BLACK;
-    graph.colors[7] = COLOR_WHITE;
+//    // Assign different colors to each data field
+//    graph.colors[0] = COLOR_RED;
+//    graph.colors[1] = COLOR_GREEN;
+//    graph.colors[2] = COLOR_BLUE;
+//    graph.colors[3] = COLOR_YELLOW;
+//    graph.colors[4] = COLOR_MAGENTA;
+//    graph.colors[5] = COLOR_CYAN;
+//    graph.colors[6] = COLOR_BLACK;
+//    graph.colors[7] = COLOR_GRAY;
+//  moved to x11_init
 
     // If the buffer is not empty, update the graph parameters based on the data
     if (buffer_size > 0) {
@@ -308,13 +324,11 @@ void update_graph() {
                 if (buffer[i].values[j] < graph.min_value) {
                     graph.min_value = buffer[i].values[j];
                 }
-
                 if (buffer[i].values[j] > graph.max_value) {
                     graph.max_value = buffer[i].values[j];
                 }
             }
         }
-
 
         // Add some margin to the minimum and maximum value
         float margin = (graph.max_value - graph.min_value) * INTERNAL_GRAPH_MARGIN;
@@ -337,11 +351,25 @@ void update_graph() {
 
 // A function to draw the graph on the window
 void draw_graph() {
+	switch (color_theme) {
+
+	case 1 :   // dark color theme
+    // Clear the window with white color
+    XSetForeground(display, gc, pixels[COLOR_BLACK]);
+    XFillRectangle(display, window, gc, 0, 0, graph.width, graph.height);
+    // Draw the x-axis and y-axis with black color
+    XSetForeground(display, gc, pixels[COLOR_WHITE]);
+	break;
+
+	default : 
     // Clear the window with white color
     XSetForeground(display, gc, pixels[COLOR_WHITE]);
     XFillRectangle(display, window, gc, 0, 0, graph.width, graph.height);
     // Draw the x-axis and y-axis with black color
     XSetForeground(display, gc, pixels[COLOR_BLACK]);
+	break;
+        }
+
 //    XDrawLine(display, window, gc, MARGIN, MARGIN, MARGIN, graph.height - MARGIN);
 //    XDrawLine(display, window, gc, MARGIN, graph.height - MARGIN, graph.width - MARGIN, graph.height - MARGIN);
     // Draw the x-axis and y-axis labels with black color
@@ -355,6 +383,8 @@ void draw_graph() {
     sprintf(label, "%.2f", graph.max_value);
     XDrawString(display, window, gc, MARGIN - MARGIN, MARGIN + 0, label, strlen(label));
 
+    float x_factor = ( (float) graph.width / (graph.max_timestamp - graph.min_timestamp) ); // calculate once to optimize loops
+    float y_factor = (graph.height - 1 * MARGIN) / (graph.max_value - graph.min_value);
     // Draw the data points and lines with different colors for each data field
     for (int i = 0; i < graph.num_fields; i++) {
         // Set the foreground color to the corresponding color for the data field
@@ -364,8 +394,10 @@ void draw_graph() {
             // Calculate the x and y coordinates of the data point on the window
 //            unsigned int x = MARGIN + (buffer[j].timestamp - graph.min_timestamp) * (graph.width - 1 * MARGIN) / (graph.max_timestamp - graph.min_timestamp);
 //            unsigned int y = graph.height - MARGIN - (buffer[j].values[i] - graph.min_value) * (graph.height - 1 * MARGIN) / (graph.max_value - graph.min_value);
-            unsigned int x = ((buffer[j].timestamp - graph.min_timestamp) * ((float) graph.width / (graph.max_timestamp - graph.min_timestamp) ));
-            unsigned int y = graph.height - MARGIN - (buffer[j].values[i] - graph.min_value) * (graph.height - 1 * MARGIN) / (graph.max_value - graph.min_value);
+//            unsigned int x = ((buffer[j].timestamp - graph.min_timestamp) * ( (float) graph.width / (graph.max_timestamp - graph.min_timestamp) ));
+            uint16_t x = ( (buffer[j].timestamp - graph.min_timestamp) * x_factor);
+//            unsigned int y = graph.height - MARGIN - (buffer[j].values[i] - graph.min_value) * (graph.height - 1 * MARGIN) / (graph.max_value - graph.min_value);
+            uint16_t y = graph.height - MARGIN - (buffer[j].values[i] - graph.min_value) * y_factor;
             // Draw a small circle around the data point
 #ifdef DATA_POINT_CIRCLE
             XFillArc(display, window, gc,
@@ -378,8 +410,10 @@ void draw_graph() {
                 // Calculate the x and y coordinates of the previous data point on the window
 //                int prev_x = MARGIN + (buffer[j-1].timestamp - graph.min_timestamp) * (graph.width - 1 * MARGIN) / (graph.max_timestamp - graph.min_timestamp);
 //                int prev_y = graph.height - MARGIN - (buffer[j-1].values[i] - graph.min_value) * (graph.height - 1 * MARGIN) / (graph.max_value - graph.min_value);
-                int prev_x = (buffer[j-1].timestamp - graph.min_timestamp) * (graph.width) / (graph.max_timestamp - graph.min_timestamp);
-                int prev_y = graph.height - MARGIN - (buffer[j-1].values[i] - graph.min_value) * (graph.height - 1 * MARGIN) / (graph.max_value - graph.min_value);
+//                uint16_t prev_x = (buffer[j-1].timestamp - graph.min_timestamp) * (graph.width) / (graph.max_timestamp - graph.min_timestamp);
+//                uint16_t prev_y = graph.height - MARGIN - (buffer[j-1].values[i] - graph.min_value) * (graph.height - 1 * MARGIN) / (graph.max_value - graph.min_value);
+                uint16_t prev_x = (buffer[j-1].timestamp - graph.min_timestamp) * x_factor;
+                uint16_t prev_y = graph.height - MARGIN - (buffer[j-1].values[i] - graph.min_value) * y_factor;
                 // Draw a line from the previous data point to this one
                 XDrawLine(display, window, gc,
                           prev_x, prev_y,
@@ -389,7 +423,19 @@ void draw_graph() {
     }
 
     // Flush the output buffer to display the graph on the window
-    XFlush(display);
+//    XFlush(display);
+}
+
+void handle_keypress(XKeyEvent *event) {
+    char buffer[16];
+    int n;
+    KeySym keysym;
+    // Get the key pressed and its symbol
+    n = XLookupString(event, buffer, sizeof(buffer), &keysym , NULL);
+    // Exit if q or Q is pressed
+    if ((n == 1) && ((buffer[0] == 'q') || (buffer[0] == 'Q'))) {
+        keypress = True;
+    }
 }
 
 // A function to handle the events from the X11 server
@@ -409,7 +455,7 @@ void handle_events() {
 
             // If it is a key press event, exit the loop
             case KeyPress:
-		keypress = True;
+		handle_keypress(&event.xkey);
                 break;
 
             // If it is a configure notify event, update the window size and redraw the graph
@@ -462,19 +508,19 @@ void serial_cb(EV_P_ ev_io *w, int revents)
 //    printf("Data point: %f\n", data_point);
 }
 
-
 // The main function of the program
 int main(int argc, char **argv) {
 
     // Check if the command line arguments are valid
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <serial device> <number of data fields>\n", argv[0]);
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <color theme number> <serial device> <number of data fields>\n", argv[0]);
         exit(1);
     }
 
     // Get the serial device name and number of data fields from the command line arguments
-    char *device = argv[1];
-    int num_fields = atoi(argv[2]);
+    color_theme = atoi(argv[1]); // TODO : implement better color theme handling. now it is simple case: hack
+    char *device = argv[2];
+    int num_fields = atoi(argv[3]);
     // Check if the number of data fields is valid
     if (num_fields < 1 || num_fields > MAX_DATA_FIELDS) {
         fprintf(stderr, "Error: Number of data fields must be between 1 and %d\n", MAX_DATA_FIELDS);
@@ -483,7 +529,7 @@ int main(int argc, char **argv) {
 
     // Initialize the X11 display and window with a title
     char title[64];
-    sprintf(title, "Real-time rolling graph from %s", device);
+    sprintf(title, "%s q to quit. ", device);
     init_x11(title);
 
     // Initialize the number of data fields in the graph
@@ -491,36 +537,35 @@ int main(int argc, char **argv) {
     // Initialize the buffer size to zero
     buffer_size = 0;
 
-    // Initialize the serial port with the device name and a baud rate of 9600
-    init_serial(device, B9600);
+    // Initialize the serial port with the device name and a baud rate 
+    init_serial(device, BAUD_RATE);
 
-   // set file descriptor as blocking
+    // set file descriptor as blocking
     fcntl(serial_fd, F_SETFL, 0);
-  // create default event loop
+    // create default event loop
     loop = ev_default_loop(0);
     // initialize io watcher for serial port file descriptor
     ev_io_init(&serial_watcher, serial_cb, serial_fd, EV_READ);
     // start io watcher
     ev_io_start(loop, &serial_watcher);
     // start event loop
-//    ev_run(loop, 0);
+    // ev_run(loop, 0);
 
         printf("discarding first data points\n");
 
         int i=0;
 	while (i < DISCARD_DATA_POINTS) {
-//	int result = read_data_point(&data_point);
+    // int result = read_data_point(&data_point);
 	while(!new_serial_data){
-//        printf("No more data\n");
+    // printf("Synchronized\n");
 	ev_run(loop,EVRUN_NOWAIT);
 		usleep(1000);
          }
 	new_serial_data = False; 
-	usleep(1000); 
+	usleep(5000); 
 	 i++;
       }
-//        printf("No more data2\n");
-
+    // printf("No more data2\n");
 
     // Loop until the user presses a key
     while (1) { // lets 
@@ -535,14 +580,14 @@ int main(int argc, char **argv) {
 		new_serial_data = False ; // reset new serial data flag
             // If the buffer is full, roll the data in the buffer
             if (buffer_size >= MAX_DATA_POINTS-2) {
-//                memcpy(buffer, buffer + sizeof(DataPoint), 8 * sizeof(DataPoint));
+            // memcpy(buffer, buffer + sizeof(DataPoint), 8 * sizeof(DataPoint));
+            // for some reason memcpy and memmove does not work. perhaps structs in buffers are padded?
 		for (int i=0; i<buffer_size; i++) {
     			buffer[i] = buffer[i+1]; 
 		        }
                 buffer_size--;
             }
             // Update the graph parameters based on the buffer
-//		printf("Test");
             update_graph();
             // Draw the graph on the window
 	// TODO: make it being called periodically on Vsync instead all the time
@@ -551,16 +596,15 @@ int main(int argc, char **argv) {
 
         else if (!new_serial_data) {
             // If no data , check if there are new events
-	usleep(2000); // sleep a little.... 
+	usleep(1000); // sleep a little.... 
 	ev_run(loop,EVRUN_NOWAIT); // poll for new serial data
-
         }
 
  //       else if (result == -1) {
             // If error, exit the program
  //           exit(1);
  //       }
-	usleep(5000); // sleep a little.... 
+//	usleep(5000); // sleep a little.... 
 //            update_graph();
 //            draw_graph();
 
